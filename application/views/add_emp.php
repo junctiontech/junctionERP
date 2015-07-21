06fbd3922345eef68f098f8520a546f3534fcdc3
Palak <palakmeshram@ymail.com>
2015-07-21 12:24:29 +0530

    remove the multi select issue

--- application/views/add_emp.php
+++ application/views/add_emp.php
@@ -36,18 +36,7 @@ $su = $userdata['role_id'];
 			<script type="text/javascript">
 				jQuery(document).ready(function($)
 				{
-					$(".multi-select").multiSelect({
-						afterInit: function()
-						{
-							// Add alternative scrollbar to list
-							this.$selectableContainer.add(this.$selectionContainer).find('.ms-list').perfectScrollbar();
-						},
-						afterSelect: function()
-						{
-							// Update scrollbar size
-							this.$selectableContainer.add(this.$selectionContainer).find('.ms-list').perfectScrollbar('update');
-						}
-					});
+				
 					
 					$(".selectboxit").selectBoxIt().on('open', function()
 					{
