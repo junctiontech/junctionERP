410976a8cf8c6da9b6253b4efc340a7586086ef0
ankit <dev2@junctiontech.in>
2015-10-21 15:32:16 +0530

    update code

--- application/controllers/user_management.php
+++ application/controllers/user_management.php
@@ -12,13 +12,12 @@ class User_management extends CI_Controller {
 	}
 	
 	function clone_db()
-	{ //echo'hiiii';
-		
+	{ 
 		$json_data=$_GET['data'];
 		$var=json_decode($json_data);
 		$database_name=$var->db_name;
-	//	$this->session->set_userdata('db_name',$database_name);
-	//	$this->session->userdata('db_name');
+		$this->session->set_userdata('db_name',$database_name);
+		$this->session->userdata('db_name');
 		$json_data=$_GET['data'];	
 		$set_user=$this->data['set_user']=$this->user_management_model->clone_db($database_name);
 		redirect('user_management/set_user?data='.$json_data);
