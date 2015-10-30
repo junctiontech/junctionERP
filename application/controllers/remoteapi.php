040bbd22db9a4a019040de6d845db6cb06aef660
ankit <dev2@junctiontech.in>
2015-10-30 16:00:34 +0530

    test

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -1,18 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /* Controller for login Functionality */
-class Remoteapi extends CI_Controller {
+class Remoteapi {
 
-	function __construct() {
-		parent::__construct();
-		$this->data[]="";
-		$this->data['user_data']="";
-		$this->data['url'] = base_url();
-		//$this->load->model('remoteapi_model');
-		$this->load->helper('url');
-		$this->data['base_url']=base_url();
-		$this->load->library('session');
-	}
 	
 	function locationUpdate(){
 		echo 'hiii';die;
