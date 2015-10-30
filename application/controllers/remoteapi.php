0a0157a72c20eb543a0e5d862d9356313ac87bd3
ankit <dev2@junctiontech.in>
2015-10-30 16:06:42 +0530

    test

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -1,8 +1,21 @@
 <?php 
 
 /* Controller for login Functionality */
-class Remoteapi {
-
+class Remoteapi extends CI_Controller {
+	
+	function __construct() {
+		parent::__construct();
+		$this->data[]="";
+		$this->data['user_data']="";
+		$this->data['url'] = base_url();
+		$this->load->model('login_model');
+		$this->load->model('remoteapi_model');
+		$this->load->library('parser');
+		$this->load->helper('url');
+		$this->load->library('form_validation');
+		$this->data['base_url']=base_url();
+		$this->load->library('session');
+	}
 	
 	function locationUpdate(){
 		echo 'hiii';die;
