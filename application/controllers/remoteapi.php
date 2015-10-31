fbab136ac4abe7e4ab190dea10b408b4155c7118
ankit <dev2@junctiontech.in>
2015-10-31 13:33:11 +0530

    fix bug

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -1,46 +1,9 @@
 <?php 
-echo 'hii';
-/* Class for Rempte Api Functionality */
-class Remoteapi {
 
-	/*function __construct() {
-		parent::__construct();
-		$this->load->model('remoteapi_model');
-		$this->load->helper('url');
-	}*/
+/* Controller for login Functionality */
+class Remoteapi {
 	
 	function locationUpdate(){
-		/*$this->load->library('session'); //if it's not autoloaded in your CI setup
-		//$this->session->set_userdata('db_name','hr');
-		//$this->session->userdata('db_name');
-		$data=json_decode($_POST['employeeData']);
-		print_r($data);die;
-		if($data)
-		{
-			$result=$this->remoteapi_model->locationUpdate($data=false);
-			/*if($result)
-			{
-				$data=array(
-				 	
-				'code'=>'200',
-				'result'=>'true',
-				);
-				print_r($data);die;
-				echo 'true'; die;
-			}
-			else
-			{
-				$data=array(
-						
-				'code'=>'400',
-				'result'=>'false',
-				);
-				echo 'false'; die;
-			}
-		}*/
-		
-
-
 		$CONNECTION=mysqli_connect("localhost",'root','bitnami','hr');
 		if($CONNECTION)
 		{
@@ -75,7 +38,7 @@ class Remoteapi {
 		{
 			echo 'connection error';
 		}
-	} 
+	}
 	
 	function task_update()
 	{
