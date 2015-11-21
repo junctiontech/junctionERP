52fea0293be0a3a9f697371860a6b53db0c6c61d
ankit <dev2@junctiontech.in>
2015-11-21 15:50:56 +0530

    update function for image api

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -39,10 +39,32 @@ class Remoteapi{
 			echo 'connection error';
 		}
 	}	
-	/* Function For Moblie Application Registration */
-	function employeeRegister()
+	/* Function for Image Update For Androide Application */
+	function image_project_update()
 	{
-		echo $_GET['employeeName'];echo $_GET['employeeMobileNumber'];echo $_GET['employeePassword'];echo $_GET['employeeOrganizationName'];echo $_GET['employeeIMEI']; die;
+		$CONNECTION=mysqli_connect("localhost",'root','bitnami','junction_erp');
+		if($CONNECTION)
+		{
+			$data=json_decode($json);
+			foreach($data as $value)
+			{
+				$query="insert into project_image(project_id,task_id,image) values ('".$value->project_id."','".$value->task_id."','".$value->image."')";
+				$sql=mysqli_query($CONNECTION,$query);
+				if($sql)
+				{
+					$image=move_uploaded_file($_FILES['image']['tmp_name'], "project_image".$value->image);
+					echo 'Image Successfully Insert';
+				}
+				else
+				{
+					echo 'Image Not Insert';
+				}
+			}
+		}
+		else
+		{
+			echo 'Server Error Connection Not Found';
+		}
 	}
 	function task_update()
 	{
