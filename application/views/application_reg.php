11b2b078150b8457c53a3ceebc59176bd55cbf3e
ankit <dev2@junctiontech.in>
2015-11-24 16:34:56 +0530

    update code for erp

--- application/views/application_reg.php
+++ application/views/application_reg.php
@@ -92,10 +92,10 @@
 															<td><?php echo $list->imei;?></td>
 															<td><?php echo $list->status;?></td>
 															<td>
-																<a href="<?php echo base_url(); ?>master/add_organization/<?=$list->organization_id; ?>" class="btn btn-secondary btn-sm btn-icon icon-left">
+																<a href="<?php echo base_url(); ?>employee/add_organization/<?=$list->registration_id ?>" class="btn btn-secondary btn-sm btn-icon icon-left">
 																		Edit
 																</a>
-																<a href="<?php echo base_url(); ?>master/delete_organization/<?=$list->organization_id; ?>" onClick="return confirm('Are you sure to delete this organization ? This will delete all the related records on this organization as well.')" class="btn btn-danger btn-sm btn-icon icon-left">
+																<a href="<?php echo base_url(); ?>employee/delete_organization/<?=$list->registration_id; ?>" onClick="return confirm('Are you sure to delete this organization ? This will delete all the related records on this organization as well.')" class="btn btn-danger btn-sm btn-icon icon-left">
 																		Delete
 																</a>
 															</td>
